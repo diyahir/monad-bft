320fd3d1f5e096044fbd4f8fa4eb8938f1d22009
Ariq <ariqchowdhury@gmail.com>
2025-08-24 19:20:18 -0400

    Ensure statesync populates 2*delay blocks in CommittedBlkBuffer
    This is a requirement for TFM reserve balance validation

--- monad-state/src/lib.rs
+++ monad-state/src/lib.rs
@@ -1255,6 +1255,7 @@ where
         let mut commands = Vec::new();
 
         let delay = self.consensus_config.execution_delay;
+        /// TFM reserve balance checking requires N-2*state_root_delay blocks to validate N
         let last_delay_committed_blocks: Vec<_> = root_parent_chain
             .iter()
             .map(|full_block| {
@@ -1293,7 +1294,7 @@ where
                     )
                     .expect("majority committed invalid block")
             })
-            .take(delay.0 as usize)
+            .take(delay.0.saturating_mul(2) as usize)
             .rev()
             .collect();
 
